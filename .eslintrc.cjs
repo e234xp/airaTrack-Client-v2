ae8360f94a7ad32bb1686209f696ee1f1b29cc44
Gavin <gavin@inncom.cloud>
2023-07-06 10:36:00 +0800

    init + gitignore

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-strongly-recommended',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-use-before-define': ['error', { functions: false }],
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    'max-len': ["error", { "ignoreStrings": true,"ignoreTemplateLiterals": true }]    
+  },
+};
+
